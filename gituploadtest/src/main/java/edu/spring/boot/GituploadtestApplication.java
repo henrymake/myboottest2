3e9120c8c671ff41f342db84f29d0f39d36e959f
henrymake <henrymake.bhs01234567890@gmail.com>
2021-09-15 01:49:51 +0900

    1 commit

---
+++ gituploadtest/src/main/java/edu/spring/boot/GituploadtestApplication.java
@@ -0,0 +1,14 @@
+package edu.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GituploadtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GituploadtestApplication.class, args);
+		System.out.println("hello github");
+	}
+
+}
